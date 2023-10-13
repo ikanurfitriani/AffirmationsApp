fd0e8230e5125dab6c37c50645b2a54a665beea4
ikanurfitriani <ikan07393@gmail.com>
2023-10-13 23:12:50 +0700

    Initial commit

---
+++ app/src/main/java/com/ikanurfitriani/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.ikanurfitriani.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
